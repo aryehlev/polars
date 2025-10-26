4a4ad8c994d2261e33cd0148c27263b6704397cb
aryehlev <aryehlev@users.noreply.github.com>
2025-10-26 21:16:46 +0200

    remove old logic.

--- crates/polars-python/src/lazyframe/general.rs
+++ crates/polars-python/src/lazyframe/general.rs
@@ -1543,28 +1543,6 @@ impl PyLazyFrame {
         Ok(out.into())
     }
 
-    /// Apply the transformations from this LazyFrame to a new data source.
-    /// This allows you to serialize just the transformation plan and apply it to different data.
-    fn apply_to_source(&self, other: &PyLazyFrame) -> PyResult<Self> {
-        let ldf = self.ldf.read();
-        let other_ldf = other.ldf.read();
-        let new_plan = ldf
-            .logical_plan
-            .apply_to_source(other_ldf.logical_plan.clone());
-        Ok(LazyFrame::from(new_plan).into())
-    }
-
-    /// Apply the transformations from this LazyFrame to a DataFrame.
-    /// This allows you to serialize just the transformation plan and apply it to different data.
-    fn apply_to_dataframe(&self, df: &PyDataFrame) -> PyResult<Self> {
-        let ldf = self.ldf.read();
-        let df_clone = df.df.read().clone();
-        let plan = ldf
-            .logical_plan
-            .apply_to_source(polars_plan::dsl::DslBuilder::from_existing_df(df_clone).build());
-        Ok(LazyFrame::from(plan).into())
-    }
-
     fn hint_sorted(
         &self,
         columns: Vec<String>,
